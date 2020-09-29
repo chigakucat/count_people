c4b1959845d9212f2470fa806992520a5f47e293
chigakucat <35723931+chigakucat@users.noreply.github.com>
2020-09-30 00:26:16 +0900

    dev

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
+# _*_ coding: utf-8 _*_
 import tkinter as tk
-import sys
-import tkinter.filedialog
 
-class TkinterClass:
-    def __init__():
-        root = tk.Tk()
-        root.option_add("*font", ('', 14))
+root = tk.Tk()
+root.title(u"People Count")
+root.geometry('400x300')
+counter = 0
 
-# variable 用のオブジェクト
-action = tk.IntVar()
-action.set(0)
-level = tk.IntVar()
-level.set(1)
 
-# ダミー
-def start(): pass
+button = tk.Button(text="インクリメント", height = 5, width = 20)
+resetButton = tk.Button(text="reset")
 
-# メニューバー
-menubar = tk.Menu(root)
-root.configure(menu = menubar)
+def clicked():
+    global counter, button
+    counter = counter + 1
 
-games = tk.Menu(menubar, tearoff = False)
-levels = tk.Menu(menubar, tearoff = False)
-menubar.add_cascade(label="ファイル", underline = 0, menu=games)
-menubar.add_cascade(label="ヘルプ", underline = 0, menu=levels)
+def reset():
+    global counter,resetButton
+    counter = 0
+    button.config(text=str(counter))
 
-# ファイル
-games.add_command(label = "開く", underline = 0, )
-games.add_command(label = "保存", underline = 0, )
-games.add_command(label = "名前を付けて保存",  underline= 0, )
-games.add_command(label = "exit", underline = 0, command = sys.exit)
+button.config(command=clicked)
+resetButton.config(command=reset)
 
-# ヘルプ
-levels.add_radiobutton(label = '使用方法', variable = level, value = 1)
-
-# ラベル
-tk.Label(root, text = "***** Menu Test *****").pack()
-
-root.mainloop()
+button.pack()
+resetButton.pack()
+button.mainloop()
+resetButton.mainloop()
